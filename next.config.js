751882bc48b73951ea43381305f2bbd9aafffd18
khalilsf205 <khalilsf205@gmail.com>
2025-03-17 14:48:22 +0100

    .

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   transpilePackages: ['next-themes'],
     reactStrictMode: true,
     swcMinify: true,
+    experimental: {
+      useDeploymentId: true,
+      // Optionally, use with Server Actions
+      useDeploymentIdServerActions: true,
+    },
   }
   
-  module.exports = nextConfig
-  
+module.exports = nextConfig;
